0c475ff89db267ae0bf9797e9e9ab14fe4e999a3
Mosh Hamedani <programmingwithmosh@gmail.com>
2023-02-28 08:02:20 -0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,34 @@
-import "./App.css";
-import ExpenseForm from "./components/ExpenseTracker/components/ExpenseForm";
-import ExpenseList from "./components/ExpenseTracker/components/ExpenseList";
-import {useState} from "react";
-import ExpenseFilter from "./components/ExpenseTracker/components/ExpenseFilter";
-import categories from "./components/ExpenseTracker/category";
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
 
 function App() {
-    const [selectedCategory, setSelectedCategory] = useState('')
+  const [count, setCount] = useState(0)
 
-    const [expenses, setExpenses] = useState([{
-        description: 'Avocado',
-        amount: 14,
-        category: 'Groceries',
-        id: Math.random()
-    },{
-        description: 'Wrench',
-        amount: 50,
-        category: 'Utilities',
-        id: Math.random()
-    },{
-        description: 'Toy Car',
-        amount: 150,
-        category: 'Entertainment',
-        id: Math.random()
-    },{
-        description: 'Toy jet',
-        amount: 200,
-        category: 'Entertainment',
-        id: Math.random()
-    },])
-
-    const visibleExpenses = selectedCategory ? expenses.filter(e => e.category === selectedCategory) : expenses
   return (
-    <div>
-        <div className="mb-5">
-            <ExpenseForm onSubmit={(expense) => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
-        </div>
-        <div className="mb3">
-        <ExpenseFilter onSelectCategory={(categories) => setSelectedCategory(categories)}/>
-        </div>
-      <ExpenseList expenses={visibleExpenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}/>
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
