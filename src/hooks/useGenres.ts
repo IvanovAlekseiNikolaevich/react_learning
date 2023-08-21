3894bce56831eee8f71d75f663c28988edabb4ba
aleksejivanov <alekseiivanov.2016@mail.ru>
2023-08-21 15:49:54 +0300

    refactoring useGenres.ts with useQuery

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import genres from "../data/genres";
+import {useQuery} from "@tanstack/react-query";
 
 export interface Genre {
   id: number;
@@ -6,6 +7,14 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+
+const useGenres = () => {
+    return useQuery<Genre[], Error>({
+            queryKey: ['genres'],
+            queryFn: () => genres,
+            staleTime: 100000,
+        }
+)
+}
 
 export default useGenres;
