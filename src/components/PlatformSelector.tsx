27284f3088c42abfabd5db020d5973c5640ea345
aleksejivanov <alekseiivanov.2016@mail.ru>
2023-08-27 15:06:38 +0300

    refactoring query objects fix

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -16,7 +16,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        {selectedPlatformId?.name || 'Platforms'}
+        {selectedPlatform?.name || 'Platforms'}
       </MenuButton>
       <MenuList>
         {data?.results.map(platform => <MenuItem onClick={() => { onSelectPlatform(platform) }} key={platform.id}>{platform.name}</MenuItem>)}
